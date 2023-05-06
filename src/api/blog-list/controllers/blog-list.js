1c881fc033b47b4ed5c0bf2e96c03cdb8103e54e
yhor333 <yhor450@gmail.com>
2023-05-06 10:50:40 +0300

    init strapi

---
+++ src/api/blog-list/controllers/blog-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog-list controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::blog-list.blog-list');
---
+++ src/api/blog-list/routes/blog-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog-list router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::blog-list.blog-list');
